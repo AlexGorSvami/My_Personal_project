9c0bcbb504e26eb2c0a07af2983f0eea67e14aa4
AlexGorSvami <jagger15696@gmail.com>
2023-03-31 18:18:38 +0500

    Start

--- First.py
+++ First.py
@@ -1,2 +1,3 @@
-print('Hello! ')
-
+cities = ["Москва", "Ульяновск", "Самара", "Уфа", "Омск", "Тула"]
+gen = (i for i in cities for j in range(1000000))
+[print(next(gen), end= ' ') for _ in range(20)]
